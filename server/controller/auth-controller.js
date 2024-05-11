2f403c453ad7f28e5ad9523950780c366786d934
vis0912 <vishalmpranav2003@gmail.com>
2024-05-11 20:11:53 +0530

    implementing simple password hashing

--- server/controller/auth-controller.js
+++ server/controller/auth-controller.js
@@ -1,4 +1,5 @@
 import User from "../models/user-model.js"
+import bcrypt from "bcryptjs"
 
 const home = async (req, res) => {
     try {
@@ -17,11 +18,14 @@ const register = async (req, res) => {
     return res.status(400).json({msg:"Email already exists"})
   }
 
+  const saltRound= await bcrypt.genSalt(10)
+  const hashedPassword= await bcrypt.hash(password,saltRound)
+
   const newUser=await User.create({
     username,
     email,
     mobile,
-    password
+    password:hashedPassword
   })
 
   res.status(200).json({ message: "Successfully created new user", user:newUser });
