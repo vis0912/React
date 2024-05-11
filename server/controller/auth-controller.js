97f0dae4c5f9f7d7234bd28a6835e7d2a11cd55b
vis0912 <vishalmpranav2003@gmail.com>
2024-05-11 19:25:41 +0530

    Code for adding new uer

--- server/controller/auth-controller.js
+++ server/controller/auth-controller.js
@@ -1,3 +1,5 @@
+import User from "../models/user-model.js"
+
 const home = async (req, res) => {
     try {
       res.status(200).send("Welcome to Home router using controller");
@@ -6,8 +8,23 @@ const home = async (req, res) => {
     }
   };
 const register = async (req, res) => {
-  console.log(req.body);
-  res.status(200).json({ message: req.body });
+
+  const {username,email,mobile,password}=req.body
+
+  const existingEmail= await User.findOne({email})
+
+  if(existingEmail){
+    return res.status(400).json({msg:"Email already exists"})
+  }
+
+  const newUser=await User.create({
+    username,
+    email,
+    mobile,
+    password
+  })
+
+  res.status(200).json({ message: "Successfully created new user", user:newUser });
 };
   
   const authController = { home,register };
