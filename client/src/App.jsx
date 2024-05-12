cec71c1efe9819f8ee6c8b9cb7711f2c82ab2f1d
vis0912 <vishalmpranav2003@gmail.com>
2024-05-12 18:45:21 +0530

    Creating pages and using react-router-dom

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,27 @@
+import {BrowserRouter,Route,Routes} from "react-router-dom"
+
+import Home from "./pages/Home"
+import About from "./pages/About"
+import Contact from "./pages/Contact"
+import Services from "./pages/Services"
+import Register from "./pages/Register"
+import Login from "./pages/Login"
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/about" element={<About />}/>
+        <Route path="/contact" element={<Contact />}/>
+        <Route path="/services" element={<Services />}/>
+        <Route path="/register" element={<Register />}/>
+        <Route path="/login" element={<Login />}/>
+      </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
