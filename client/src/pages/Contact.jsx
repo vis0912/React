0bcd6827b4c97e313505e56fb8eb147f8ec495b6
vis0912 <vishalmpranav2003@gmail.com>
2024-05-13 00:56:59 +0530

    Contact form data stored in state variable

--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -1,9 +1,99 @@
-import React from 'react'
+import React from "react";
+import { useState } from "react";
 
 function Contact() {
+  const [userContact, setUserContact] = useState({
+    username: "",
+    email: "",
+    message: "",
+  });
+
+  const handleInput = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+
+    setUserContact({
+      ...userContact,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(userContact);
+  };
+
   return (
-    <div>Contact</div>
-  )
+    <>
+      <section>
+        <main>
+          <div className="section-contact">
+            <div className="container grid grid-two-cols">
+              <div className="contact-image">
+                <img
+                  src="/images/support.png"
+                  alt="Loading"
+                  width="500"
+                  height="500"
+                />
+              </div>
+              <div className="contact-form">
+                <h1 className="main-heading mb-3">contact form</h1>
+                <br />
+
+                <form onSubmit={handleSubmit}>
+                  <div>
+                    <label htmlFor="username">username</label>
+                    <input
+                      type="text"
+                      name="username"
+                      placeholder="Enter username"
+                      id="username"
+                      required
+                      autoComplete="off"
+                      value={userContact.username}
+                      onChange={handleInput}
+                    />
+                  </div>
+                  <div>
+                    <label htmlFor="email">email</label>
+                    <input
+                      type="email"
+                      name="email"
+                      placeholder="Enter email"
+                      id="email"
+                      required
+                      autoComplete="off"
+                      value={userContact.email}
+                      onChange={handleInput}
+                    />
+                  </div>
+
+                  <div>
+                    <label htmlFor="message">message</label>
+                    <textarea
+                      name="message"
+                      placeholder="Enter message"
+                      id="message"
+                      required
+                      autoComplete="off"
+                      value={userContact.message}
+                      onChange={handleInput}
+                    />
+                  </div>
+
+                  <br />
+                  <button type="submit" className="btn btn-submit">
+                    Login Now
+                  </button>
+                </form>
+              </div>
+            </div>
+          </div>
+        </main>
+      </section>
+    </>
+  );
 }
 
-export default Contact
+export default Contact;
