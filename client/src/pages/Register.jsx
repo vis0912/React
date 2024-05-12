074a45c1c169c37909bc9d8c429ef240e6102eeb
vis0912 <vishalmpranav2003@gmail.com>
2024-05-13 01:20:25 +0530

    Finally connecting React to backend, useNavigate hook

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,28 +1,55 @@
 import React from "react";
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 function Register() {
+  const navigate = useNavigate();
   const [userRegister, setUserRegister] = useState({
-    username: "vis0912",
+    username: "",
     email: "",
     password: "",
     mobile: "",
   });
 
-  const handleInput=(e)=>{
-    const name=e.target.name
-    const value=e.target.value
+  const handleInput = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
 
     setUserRegister({
       ...userRegister,
-      [name]:value
-    })
-  }
+      [name]: value,
+    });
+  };
 
-  const handleSubmit=(e)=>{
-    e.preventDefault()
-    console.log(userRegister);
-  }
+  const handleSubmit = async (e) => {
+    try {
+      e.preventDefault();
+      console.log(userRegister);
+
+      const response = await fetch("http://localhost:5000/api/auth/register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(userRegister),
+      });
+
+      console.log(response);
+
+      if (response.ok) {
+        setUserRegister({
+          username: "",
+          email: "",
+          password: "",
+          mobile: "",
+        });
+
+        navigate("/login");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <>
--- server/server.js
+++ server/server.js
@@ -4,11 +4,20 @@ import contactRouter from "./router/contact-router.js";
 import connectDB from "./utils/db.js";
 import dotenv from "dotenv";
 import errorMiddleware from "./middleware/error-middleware.js";
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+const corsOptions = {
+  origin: "http://localhost:5173",
+  methods: "GET,POST",
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
 
 app.use(express.json());
 app.use("/api/auth", authRouter);
