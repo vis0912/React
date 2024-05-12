d17fc6af57db28fa2f95e4636f004396ac040262
vis0912 <vishalmpranav2003@gmail.com>
2024-05-12 21:50:13 +0530

    creating navbar basic

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,24 +1,27 @@
-import {BrowserRouter,Route,Routes} from "react-router-dom"
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-import Home from "./pages/Home"
-import About from "./pages/About"
-import Contact from "./pages/Contact"
-import Services from "./pages/Services"
-import Register from "./pages/Register"
-import Login from "./pages/Login"
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Services from "./pages/Services";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <>
       <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />}/>
-        <Route path="/about" element={<About />}/>
-        <Route path="/contact" element={<Contact />}/>
-        <Route path="/services" element={<Services />}/>
-        <Route path="/register" element={<Register />}/>
-        <Route path="/login" element={<Login />}/>
-      </Routes>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
       </BrowserRouter>
     </>
   );
