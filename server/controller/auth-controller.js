53ece2ac3f4ed52da18067cb5b123365e609e026
vis0912 <vishalmpranav2003@gmail.com>
2024-05-12 08:12:46 +0530

    Added login function and validation

--- server/controller/auth-controller.js
+++ server/controller/auth-controller.js
@@ -1,4 +1,5 @@
 import User from "../models/user-model.js";
+import bcrypt from "bcryptjs";
 
 const home = async (req, res) => {
   try {
@@ -23,15 +24,40 @@ const register = async (req, res) => {
     password,
   });
 
-  res
-    .status(200)
-    .json({
-      message: "Successfully created new user",
-      user: newUser,
-      token: await newUser.generateToken(),
-    });
+  res.status(200).json({
+    message: "Successfully created new user",
+    user: newUser,
+    token: await newUser.generateToken(),
+    userId: newUser._id.toString(),
+  });
 };
 
-const authController = { home, register };
+const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const existingUser = await User.findOne({ email });
+
+    if (!existingUser) {
+      return res.status(401).json({ msg: "Invalid credentials" });
+    }
+
+    const isMatch = await bcrypt.compare(password, existingUser.password);
+    if (!isMatch) {
+      return res.status(401).json({ msg: "BAD credentials" });
+    }
+
+    res.status(200).json({
+      msg: "Successfully Logged in",
+      user: existingUser.username,
+      token: await existingUser.generateToken(),
+      userId: existingUser._id.toString(),
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const authController = { home, register, login };
 
 export default authController;
--- server/router/auth-router.js
+++ server/router/auth-router.js
@@ -6,5 +6,6 @@ const router = express.Router();
 
 router.route("/").get(authController.home);
 router.route("/register").post(authController.register);
+router.route("/login").post(authController.login);
 
 export default router;
