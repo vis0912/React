ff2519b926bb755cd93285aed2d69bc2f92928a5
vis0912 <vishalmpranav2003@gmail.com>
2024-05-12 13:03:47 +0530

    Zod validation

---
+++ server/middleware/validator-middleware.js
@@ -0,0 +1,49 @@
+import { z } from "zod";
+
+// Define validation schema
+const schema = z.object({
+  username: z.string({
+    required_error: "Name is required",
+    invalid_type_error: "Name must be a string",
+  })
+  .min(3, { message: "At least 3 characters required" })
+  .trim(),
+
+  email: z.string({
+    required_error: "Email is required",
+    invalid_type_error: "Email must be a string",
+  })
+  .email({ message: "Enter a valid email address" }),
+
+  mobile: z.string({
+    required_error: "Mobile is required",
+    invalid_type_error: "Mobile must be a string",
+  })
+  .min(10, { message: "Exactly 10 digits required" })
+  .max(10, { message: "Cannot be more than 10 digits" }),
+
+  password: z.string({
+    required_error: "Password is required",
+    invalid_type_error: "Password must be a string",
+  })
+  .min(8, { message: "Password must be at least 8 characters long" }),
+});
+
+// Middleware for validation
+const validate = () => async (req, res, next) => {
+  try {
+    // Parse request body using the defined schema
+    const parsedBody = await schema.parseAsync(req.body);
+    // Replace request body with parsed body
+    req.body = parsedBody;
+    // Proceed to the next middleware
+    next();
+  } catch (error) {
+    // Handle validation errors
+    console.error(error);
+    const message = error.errors[0].message;
+    res.status(400).json({ error: message });
+  }
+};
+
+export default validate;
--- server/router/auth-router.js
+++ server/router/auth-router.js
@@ -1,11 +1,12 @@
 // routes.js
 import express from "express";
 import authController from "../controller/auth-controller.js";
+import validate from "../middleware/validator-middleware.js"
 
 const router = express.Router();
 
 router.route("/").get(authController.home);
-router.route("/register").post(authController.register);
+router.route("/register").post(validate(),authController.register);
 router.route("/login").post(authController.login);
 
 export default router;
