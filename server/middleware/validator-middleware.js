1b7a36bf15bde934902026306dc5fed07ce484a2
vis0912 <vishalmpranav2003@gmail.com>
2024-05-12 13:29:46 +0530

    login validation

--- server/middleware/validator-middleware.js
+++ server/middleware/validator-middleware.js
@@ -1,36 +1,56 @@
 import { z } from "zod";
 
 // Define validation schema
-const schema = z.object({
-  username: z.string({
-    required_error: "Name is required",
-    invalid_type_error: "Name must be a string",
-  })
-  .min(3, { message: "At least 3 characters required" })
-  .trim(),
+const registerSchema = z.object({
+  username: z
+    .string({
+      required_error: "Name is required",
+      invalid_type_error: "Name must be a string",
+    })
+    .min(3, { message: "At least 3 characters required" })
+    .trim(),
 
-  email: z.string({
-    required_error: "Email is required",
-    invalid_type_error: "Email must be a string",
-  })
-  .email({ message: "Enter a valid email address" }),
+  email: z
+    .string({
+      required_error: "Email is required",
+      invalid_type_error: "Email must be a string",
+    })
+    .email({ message: "Enter a valid email address" }),
 
-  mobile: z.string({
-    required_error: "Mobile is required",
-    invalid_type_error: "Mobile must be a string",
-  })
-  .min(10, { message: "Exactly 10 digits required" })
-  .max(10, { message: "Cannot be more than 10 digits" }),
+  mobile: z
+    .string({
+      required_error: "Mobile is required",
+      invalid_type_error: "Mobile must be a string",
+    })
+    .min(10, { message: "Exactly 10 digits required" })
+    .max(10, { message: "Cannot be more than 10 digits" }),
 
-  password: z.string({
-    required_error: "Password is required",
-    invalid_type_error: "Password must be a string",
-  })
-  .min(8, { message: "Password must be at least 8 characters long" }),
+  password: z
+    .string({
+      required_error: "Password is required",
+      invalid_type_error: "Password must be a string",
+    })
+    .min(8, { message: "Password must be at least 8 characters long" }),
+});
+
+const loginSchema = z.object({
+  email: z
+    .string({
+      required_error: "Email is required",
+      invalid_type_error: "Email must be a string",
+    })
+    .email({ message: "Enter a valid email address" }),
+
+  password: z
+    .string({
+      required_error: "Password is required",
+      invalid_type_error: "Password must be a string",
+    })
+    .min(8, { message: "Password must be at least 8 characters long" }),
 });
 
 // Middleware for validation
-const validate = () => async (req, res, next) => {
+const validate = (schema) => async (req, res, next) => {
   try {
     // Parse request body using the defined schema
     const parsedBody = await schema.parseAsync(req.body);
@@ -46,4 +66,4 @@ const validate = () => async (req, res, next) => {
   }
 };
 
-export default validate;
+export { registerSchema, loginSchema, validate };
--- server/router/auth-router.js
+++ server/router/auth-router.js
@@ -1,12 +1,18 @@
 // routes.js
 import express from "express";
 import authController from "../controller/auth-controller.js";
-import validate from "../middleware/validator-middleware.js"
+import {
+  registerSchema,
+  loginSchema,
+  validate,
+} from "../middleware/validator-middleware.js";
 
 const router = express.Router();
 
 router.route("/").get(authController.home);
-router.route("/register").post(validate(),authController.register);
-router.route("/login").post(authController.login);
+router
+  .route("/register")
+  .post(validate(registerSchema), authController.register);
+router.route("/login").post(validate(loginSchema), authController.login);
 
 export default router;
