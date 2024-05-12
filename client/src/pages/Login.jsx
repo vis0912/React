429d04ddba451971d8913eb75f83f697afa0423b
vis0912 <vishalmpranav2003@gmail.com>
2024-05-12 23:42:39 +0530

    storing  register and login form data in state variable

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,9 +1,86 @@
-import React from 'react'
+import React from "react";
+import { useState } from "react";
 
 function Login() {
+  const [userLogin, setUserLogin] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleInput = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+
+    setUserLogin({
+      ...userLogin,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(userLogin);
+  };
+
   return (
-    <div>Login</div>
-  )
+    <>
+      <section>
+        <main>
+          <div className="section-login">
+            <div className="container grid grid-two-cols">
+              <div className="login-image">
+                <img
+                  src="/images/login.png"
+                  alt="Loading"
+                  width="500"
+                  height="500"
+                />
+              </div>
+              <div className="login-form">
+                <h1 className="main-heading mb-3">LOGIN form</h1>
+                <br />
+
+                <form onSubmit={handleSubmit}>
+                  <div>
+                    <label htmlFor="email">email</label>
+                    <input
+                      type="email"
+                      name="email"
+                      placeholder="Enter email"
+                      id="email"
+                      required
+                      autoComplete="off"
+                      value={userLogin.email}
+                      onChange={handleInput}
+                    />
+                  </div>
+
+                  <div>
+                    <label htmlFor="password">password</label>
+                    <input
+                      type="password"
+                      name="password"
+                      placeholder="Enter password"
+                      id="password"
+                      required
+                      autoComplete="off"
+                      value={userLogin.password}
+                      onChange={handleInput}
+                    />
+                  </div>
+
+                  <br />
+                  <button type="submit" className="btn btn-submit">
+                    Login Now
+                  </button>
+                </form>
+              </div>
+            </div>
+          </div>
+        </main>
+      </section>
+    </>
+  );
 }
 
-export default Login
+export default Login;
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,9 +1,116 @@
-import React from 'react'
+import React from "react";
+import { useState } from "react";
 
 function Register() {
+  const [userRegister, setUserRegister] = useState({
+    username: "vis0912",
+    email: "",
+    password: "",
+    mobile: "",
+  });
+
+  const handleInput=(e)=>{
+    const name=e.target.name
+    const value=e.target.value
+
+    setUserRegister({
+      ...userRegister,
+      [name]:value
+    })
+  }
+
+  const handleSubmit=(e)=>{
+    e.preventDefault()
+    console.log(userRegister);
+  }
+
   return (
-    <div>Register</div>
-  )
+    <>
+      <section>
+        <main>
+          <div className="section-registration">
+            <div className="container grid grid-two-cols">
+              <div className="registration-image">
+                <img
+                  src="/images/register.png"
+                  alt="Loading"
+                  width="500"
+                  height="500"
+                />
+              </div>
+              <div className="registration-form">
+                <h1 className="main-heading mb-3">registration form</h1>
+                <br />
+
+                <form onSubmit={handleSubmit}>
+                  <div>
+                    <label htmlFor="username">username</label>
+                    <input
+                      type="text"
+                      name="username"
+                      placeholder="username"
+                      id="username"
+                      required
+                      autoComplete="off"
+                      value={userRegister.username}
+                      onChange={handleInput}
+                    />
+                  </div>
+
+                  <div>
+                    <label htmlFor="email">email</label>
+                    <input
+                      type="email"
+                      name="email"
+                      placeholder="Enter email"
+                      id="email"
+                      required
+                      autoComplete="off"
+                      value={userRegister.email}
+                      onChange={handleInput}
+                    />
+                  </div>
+
+                  <div>
+                    <label htmlFor="mobile">mobile</label>
+                    <input
+                      type="number"
+                      name="mobile"
+                      placeholder="Enter mobile"
+                      id="mobile"
+                      required
+                      autoComplete="off"
+                      value={userRegister.mobile}
+                      onChange={handleInput}
+                    />
+                  </div>
+
+                  <div>
+                    <label htmlFor="password">password</label>
+                    <input
+                      type="password"
+                      name="password"
+                      placeholder="Enter password"
+                      id="password"
+                      required
+                      autoComplete="off"
+                      value={userRegister.password}
+                      onChange={handleInput}
+                    />
+                  </div>
+
+                  <br />
+                  <button type="submit" className="btn btn-submit">
+                    Register Now
+                  </button>
+                </form>
+              </div>
+            </div>
+          </div>
+        </main>
+      </section>
+    </>
+  );
 }
 
-export default Register
+export default Register;
